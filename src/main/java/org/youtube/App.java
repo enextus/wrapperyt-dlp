98aa7ab73c0fbedc7c69a583e40fef49d0ec6edb
Your Name <you@example.com>
2023-07-29 09:52:15 +0200

    0.3.0

---
+++ src/main/java/org/youtube/App.java
@@ -0,0 +1,15 @@
+package org.youtube;
+
+import javax.swing.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            VideoDownloaderFrame frame = new VideoDownloaderFrame();
+            frame.setVisible(true);
+        });
+    }
+
+}
---
+++ src/main/java/org/youtube/VideoDownloaderFrame.java
@@ -0,0 +1,75 @@
+package org.youtube;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoDownloaderFrame extends JFrame {
+
+    private final JTextField urlField;
+    private final JButton downloadButton;
+    private final JLabel infoLabel;
+
+    public VideoDownloaderFrame() {
+        setTitle("Video Downloader");
+        setSize(500, 120);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // Центрируем окно
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        urlField = new JTextField();
+        downloadButton = new JButton("Download");
+        infoLabel = new JLabel("Enter the video URL");
+
+        downloadButton.addActionListener(e -> downloadVideo());
+
+        panel.add(urlField, BorderLayout.CENTER);
+        panel.add(downloadButton, BorderLayout.EAST);
+        panel.add(infoLabel, BorderLayout.SOUTH);
+
+        add(panel);
+    }
+
+    private void downloadVideo() {
+        String url = urlField.getText();
+        String outputPath = "C:/Users/Administrator/Desktop/"; // Путь для сохранения видео
+
+        List<String> command = new ArrayList<>();
+        command.add("yt-dlp");
+        command.add(url);
+        command.add("-o");
+        command.add(outputPath + "%(title)s.%(ext)s");
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        processBuilder.redirectErrorStream(true);  // Объединяем стандартный поток вывода и поток ошибок
+
+        try {
+            Process process = processBuilder.start();
+
+            // Читаем поток вывода
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            process.waitFor();
+
+            // Если в выводе есть сообщение об ошибке, отображаем его
+            if (output.toString().contains("ERROR")) {
+                infoLabel.setText("Error: Check output");
+                System.out.println(output);  // Выводим всю информацию в консоль
+            } else {
+                infoLabel.setText("Download complete");
+            }
+        } catch (Exception ex) {
+            infoLabel.setText("Error: " + ex.getMessage());
+        }
+    }
+
+}
